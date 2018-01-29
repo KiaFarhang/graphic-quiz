0e4e948c11a5bdcd92ab5bd14e3412ea6abbd058
Kia Farhang <kfarhang0@gmail.com>
2018-01-29 10:54:12 -0600

    Finish implementation of Redux-connected CurrentQuestion component

--- src/components/containers/CurrentQuestion.tsx
+++ src/components/containers/CurrentQuestion.tsx
@@ -1,22 +1,27 @@
 import { connect, Dispatch } from 'react-redux';
-import { Question } from '../Question';
+import Question from '../Question';
 import * as types from '../../types';
 import { AddPoints, IncrementQuestionIndex, addPoints, incrementQuestionIndex } from '../../actions';
 
 const questions: types.Question[] = require('../../questions.json');
 
-const mapStateToProps = (state: types.AppState) => ({
-    img: questions[state.currentQuestionIndex].img,
-    answers: questions[state.currentQuestionIndex].answers,
-    correctAnswerIndex: questions[state.currentQuestionIndex].correctAnswerIndex
-});
+const mapStateToProps = (state: types.AppState) => {
+    return {
+        img: questions[state.currentQuestionIndex].img,
+        answers: questions[state.currentQuestionIndex].answers,
+        correctAnswerIndex: questions[state.currentQuestionIndex].correctAnswerIndex,
+    };
+};
 
-const mapDispatchToProps = (dispatch: Dispatch<AddPoints | IncrementQuestionIndex>) => ({
-    addPoints: (points: number) => dispatch(addPoints(points)),
-    incrementQuestionIndex: () => dispatch(incrementQuestionIndex())
-});
+const mapDispatchToProps = (dispatch: Dispatch<AddPoints | IncrementQuestionIndex>) => {
+    return {
+        addPoints: (points: number) => dispatch(addPoints(points)),
+        incrementQuestionIndex: () => dispatch(incrementQuestionIndex()),
+    };
+};
 
 export default connect(
     mapStateToProps,
-    mapDispatchToProps
-)(Question)
+    mapDispatchToProps)
+    // tslint:disable-next-line 
+    (Question as any);
