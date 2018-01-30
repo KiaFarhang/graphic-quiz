828e207454fb10a3d94583ea56d318c5ccdcffec
Kia Farhang <kfarhang0@gmail.com>
2018-01-30 10:18:47 -0600

    Add Redux wiring to top-level index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { Provider } from 'react-redux';
 import App from './components/App';
+
+import { quizApp } from './reducers';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = createStore(quizApp, composeWithDevTools());
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('graphic-quiz-root') as HTMLElement
 );
 registerServiceWorker();
