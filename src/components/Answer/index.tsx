5fa5e952976710e055447d57dc8864e95f66c5b1
Kia Farhang <kfarhang0@gmail.com>
2018-01-30 13:50:40 -0600

    Add light styling to show correct/incorrect selected answers

--- src/components/Answer/index.tsx
+++ src/components/Answer/index.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import { Answer as Props } from '../../types';
 
+import './Answer.css';
+
 const Answer = ({ text, selected = false, correct, action }: Props) => {
+    // tslint:disable-next-line 
+    console.log('rendering answer');
+    let answerClass = 'answer';
+    if (selected) {
+        answerClass += ' selected';
+        let correctStatus = correct ? ' correct' : ' incorrect';
+        answerClass += correctStatus;
+    }
     return (
-        <p onClick={action ? action() : null}>{text}</p>
+        <p onClick={action ? action() : null} className={answerClass}>{text}</p>
     );
 };
 
