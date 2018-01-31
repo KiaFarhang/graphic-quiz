22e49a028dd0a9333611bb4bb55e8f6bce4b4db6
Kia Farhang <kfarhang0@gmail.com>
2018-01-31 11:49:16 -0600

    Refactor Question component

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,38 +1,33 @@
 import * as React from 'react';
-import { Question as Props, Answer as AnswerProps } from '../../types';
-import Answer from '../Answer';
+import { QuestionComponent as Props, Answer as AnswerProps } from '../../types';
+import ConnectedAnswer from '../containers/ConnectedAnswer';
 
-export interface State {
-    selectedAnswers: number[];
-    pointsPossible: number;
-}
+import './Question.css';
 
-class Question extends React.Component<Props, State>{
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            selectedAnswers: [],
-            pointsPossible: 3
-        }
-    }
-
-    render() {
-
-        const answers = this.props.answers.map((answer: AnswerProps, index: number) => {
-            return <Answer
-                text={answer.text}
-                selected={this.state.selectedAnswers.indexOf(index) > -1 ? true : false}
-                correct={this.props.correctAnswerIndex === index}
-            />
-        });
-
+const Question = ({ answers, answerFunction, correctAnswerIndex, img, selectedAnswers }: Props) => {
+    const answerComponents = answers.map((answer: AnswerProps, index) => {
+        const currentAnswer = answers[index];
         return (
-            <div className="Question">
-                <img src={this.props.img} alt="" />
-                {answers}
+            <ConnectedAnswer
+                correct={correctAnswerIndex === index}
+                index={index}
+                key={index}
+                selected={selectedAnswers.indexOf(index) >= 0}
+                text={currentAnswer.text}
+            />
+        );
+    });
+
+    return (
+        <div className="question">
+            <div className="imgContainer">
+                <img src={img} alt="" />
             </div>
-        )
-    }
-}
+            <div className="answerContainer">
+                {answerComponents}
+            </div>
+        </div>
+    );
+};
 
 export default Question;
--- src/components/containers/CurrentQuestion.tsx
+++ src/components/containers/CurrentQuestion.tsx
@@ -1,27 +1,48 @@
 import { connect, Dispatch } from 'react-redux';
 import Question from '../Question';
+import { Question as QuestionProps } from '../../types';
 import * as types from '../../types';
-import { AddPoints, IncrementQuestionIndex, addPoints, incrementQuestionIndex } from '../../actions';
-
-const questions: types.Question[] = require('../../questions.json');
+import { SendAnswer, sendAnswer } from '../../actions';
 
 const mapStateToProps = (state: types.AppState) => {
+    const currentQuestion: QuestionProps = state.questions[state.currentQuestionIndex];
+
     return {
-        img: questions[state.currentQuestionIndex].img,
-        answers: questions[state.currentQuestionIndex].answers,
-        correctAnswerIndex: questions[state.currentQuestionIndex].correctAnswerIndex,
+        answers: currentQuestion.answers,
+        correctAnswerIndex: currentQuestion.correctAnswerIndex,
+        img: currentQuestion.img,
+        selectedAnswers: state.selectedAnswers
     };
 };
 
-const mapDispatchToProps = (dispatch: Dispatch<AddPoints | IncrementQuestionIndex>) => {
+const mapDispatchToProps = (dispatch: Dispatch<SendAnswer>) => {
     return {
-        addPoints: (points: number) => dispatch(addPoints(points)),
-        incrementQuestionIndex: () => dispatch(incrementQuestionIndex()),
+        answerFunction: (index: number) => dispatch(sendAnswer(index))
     };
 };
+
+// const mapStateToProps = (state: types.AppState) => {
+//     return {
+//         img: questions[state.currentQuestionIndex].img,
+//         answers: questions[state.currentQuestionIndex].answers,
+//         correctAnswerIndex: questions[state.currentQuestionIndex].correctAnswerIndex,
+//     };
+// };
+
+// const mapDispatchToProps = (dispatch: Dispatch<AddPoints | IncrementQuestionIndex>) => {
+//     return {
+//         addPoints: (points: number) => dispatch(addPoints(points)),
+//         incrementQuestionIndex: () => dispatch(incrementQuestionIndex()),
+//     };
+// };
+
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps)
+//     // tslint:disable-next-line 
+//     (Question as any);
 
 export default connect(
     mapStateToProps,
-    mapDispatchToProps)
-    // tslint:disable-next-line 
-    (Question as any);
+    mapDispatchToProps
+)(Question);
